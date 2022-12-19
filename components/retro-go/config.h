02e08ca01a83847e6d6f7604bdfb40938d62fbc0
Andreqp <76157443+Andreqp@users.noreply.github.com>
2022-12-19 11:23:41 +0300

    Update config.h
    add ESPLAY_S3

--- components/retro-go/config.h
+++ components/retro-go/config.h
@@ -10,6 +10,8 @@
 #include "targets/sdl2/config.h"
 #elif defined(RG_TARGET_MRGC_GBM)
 #include "targets/mrgc-gbm/config.h"
+#elif defined(RG_TARGET_ESPLAY_S3)
+#include "targets/esplay-s3/config.h"
 #else
 #warning "No target defined. Defaulting to ODROID-GO."
 #include "targets/odroid-go/config.h"
